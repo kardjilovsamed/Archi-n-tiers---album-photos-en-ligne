c98b0e7463470598bcda0236a7c5fcf8b9739bed
kardjilovsamed <samed.kardjilov@gmail.com>
2016-01-27 10:11:10 +0100

    Avancement script explorateur et html

--- public/js/scriptExplorateur.js
+++ public/js/scriptExplorateur.js
@@ -1,9 +1,68 @@
 $(document).ready(function () {
 
-    getEvent();
+    getAlbums();
+
+    var token = localStorage.getItem("token");
+    var idAlbumRoot = localStorage.getItem("idAlbumRoot");
+
+    $('a .album').click(function(){
+        alert('ALBUM !!!!!!!!!');
+        idAlbumRoot = $(this).attr('href');
+
+        $("#explorateur").empty();
+        getAlbums();
+
+    });
+
+    $("#addButton").click(function(){
+
+        var folderName = prompt("Nom du nouveau dossier", "Nom");
+
+        if (folderName != null) {
+            $("#explorateur").prepend(  '<div class="col-md-2">' +
+                                            '<a href="onvera.com" class="album"> ' +
+                                                '<i class="fa fa-folder fa-lg"></i><br/>' + folderName +
+                                            '</a>' +
+                                        '</div>')
+        }
 
 
-    function getEvent() {
+    });
+
+
+    function getAlbums() {
+        $.ajax({
+                type: "GET",
+                url: "/albums/" + idAlbumRoot,
+                dataType: "json",
+                encode: true
+            })
+            .success(function (data) {
+                alert("Success");
+
+                $.each(data, function (index, item) {
+
+                    var lienAlbumEnfant = item.uri;
+
+
+                    $("#explorateur").append( '<div class="col-md-2">' +
+                                                    '<a href="' + item.id + '" class="album"> ' +
+                                                        '<i class="fa fa-folder fa-lg"></i><br/>' + item.nom +
+                                                    '</a>' +
+                                                '</div>')
+
+                });
+
+                getImages();
+
+            })
+            .error(function (data) {
+                alert("Error, impossible de se connecter");
+            });
+    }
+
+    function getImages() {
+
         $.ajax({
                 type: "GET",
                 url: "/photos/test",
@@ -12,11 +71,26 @@ $(document).ready(function () {
             })
             .success(function (data) {
                 alert("Success");
+
+                $.each(data, function (index, item) {
+
+                    var lienImageGrand = item.uri;
+                    var lienImage = item.uri;
+
+                    $("#explorateur").append( '<div class="col-md-2">' +
+                                                    '<a href="' + lienImageGrand + '" > ' +
+                                                        '<img src="' + lienImage + '" class="img-responsive"/>' +
+                                                    '</a>' +
+                                                '</div>')
+
+                });
             })
             .error(function (data) {
                 alert("Error, impossible de se connecter");
             });
     }
+
+
 
 });
 
