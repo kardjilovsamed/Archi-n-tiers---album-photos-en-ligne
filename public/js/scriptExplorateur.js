f88eb2de6cf18926715ffa9ad71ba68fd5ead0b8
kardjilovsamed <samed.kardjilov@gmail.com>
2016-01-31 17:51:57 +0100

    Gestion token

--- public/js/scriptExplorateur.js
+++ public/js/scriptExplorateur.js
@@ -1,49 +1,22 @@
 $(document).ready(function () {
 
-   // getAlbums();
+    getAlbums();
 
     var token = localStorage.getItem("token");
-    var idAlbumRoot = localStorage.getItem("idAlbumRoot");
-
-
-
-    var isDragging = false;
-    $(".img-responsive")
-        .mousedown(function() {
-            isDragging = false;
-        })
-        .mousemove(function() {
-            isDragging = true;
-        })
-        .mouseup(function() {
-            var wasDragging = isDragging;
-            isDragging = false;
-            if (!wasDragging) {
-                $('#imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
-                $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-            }
-        });
-
+    var idAlbumRoot = localStorage.getItem("albumRoot");
 
     $(document).on('click', '.breadcrumb li', function(){
-
         idAlbumRoot = $(this).attr('id');
-
-        //$("#explorateur").empty();
-        //getAlbums();
-
-
+        $("#explorateur").empty();
+        getAlbums();
         while($('#breadcrumb').children().last().attr('id') != $(this).attr('id')){
             $('#breadcrumb').children().last().remove();
         }
-
-
         var foldernameFormer = $('#breadcrumb').children().last().text();
         var folderIdFormer = $('#breadcrumb').children().last().attr('id');
 
         $('#breadcrumb').children().last().remove();
         $("#breadcrumb").append( '<li id="' + folderIdFormer + '" class="active">' + foldernameFormer + '</li>');
-
 
     });
 
@@ -51,24 +24,36 @@ $(document).ready(function () {
 
         idAlbumRoot = $(this).attr('id');
 
-
         var foldernameFormer = $('#breadcrumb li.active').text();
         var folderIdFormer = $('#breadcrumb li.active').attr('id');
 
         $('#breadcrumb li.active').remove();
-
         $('#breadcrumb').append( '<li id="' + folderIdFormer + '"><a href="#" class="breadcrumbLink">' + foldernameFormer + '</a></li>');
-
         $("#breadcrumb").append( '<li id="' + idAlbumRoot + '" class="active">' + $(this).find('a:first').text() + '</li>');
 
-
-        //$("#explorateur").empty();
-
-        //getAlbums();
+        $("#explorateur").empty();
+        getAlbums();
 
         return false;
     });
 
+    $(document).on('click', '.albumParent', function(){
+
+        idAlbumRoot = $(this).attr('id');
+
+        $('#breadcrumb').children().last().remove();
+        var foldernameFormer = $('#breadcrumb').children().last().text();
+        var folderIdFormer = $('#breadcrumb').children().last().attr('id');
+        $('#breadcrumb').children().last().remove();
+        $("#breadcrumb").append( '<li id="' + folderIdFormer + '" class="active">' + foldernameFormer + '</li>');
+
+
+        $("#explorateur").empty();
+        getAlbums();
+
+
+        return false;
+    });
 
     $("#addButton").click(function(){
 
@@ -90,17 +75,15 @@ $(document).ready(function () {
     function getAlbums() {
         $.ajax({
                 type: "GET",
-                url: "/albums/" + idAlbumRoot,
+                url: "/albums/" + idAlbumRoot + '?access_token='+ token,
                 dataType: "json",
                 encode: true
             })
             .success(function (data) {
-                alert("Success");
 
                 $.each(data, function (index, item) {
 
                     var lienAlbumEnfant = item.uri;
-
 
                     $("#explorateur").append( '<div class="col-md-2">' +
                                                     '<a href="' + item.id + '" class="album"> ' +
@@ -109,6 +92,16 @@ $(document).ready(function () {
                                                 '</div>')
 
                 });
+
+                var lienParent = data.parentAlbum.uri;
+
+                if(lienParent){
+                    $("#explorateur").prepend(  '<div id="' + data.parentAlbum.id + '" class="col-md-2 albumParent dropzone draggable drag-drop yes-drop">' +
+                                                    '<a href="onvera.com">' +
+                                                        '<i class="fa fa-folder fa-lg"></i><br/>' + data.parentAlbum.name +
+                                                    '</a>' +
+                                                '</div>' );
+                }
 
                 getImages();
 
@@ -122,7 +115,7 @@ $(document).ready(function () {
 
         $.ajax({
                 type: "GET",
-                url: "/photos/test",
+                url: "/photos/test" + '?access_token='+ token,
                 dataType: "json",
                 encode: true
             })
