9692c57a48d3f16966a06dbc4e6c4deb7069b55c
Aurélien Touzet <aurelien.touzet.42@gmail.com>
2016-02-07 15:53:39 +0100

    url image

--- routes/search.js
+++ routes/search.js
@@ -7,8 +7,12 @@ var Album = require('../models/albums');
 var Photo = require('../models/photos');
 
 router.get('/images', passport.authenticate('bearer', { session: false }), function (req, res, next) {
-    if(req.query.user)
-    Album.findById(req.params.id, function(err, album) {
+    var query = {};
+    if(req.query.owner)
+        query.owner = req.query.owner;
+    if(req.query.tag)
+        query.tags = {$regex: req.query.tag, $options: "i"};
+    Album.find(req.params.id, function(err, album) {
         if (err) return next(err);
         if(album.owner == req.user.id || (album.permissions && album.permissions.indexOf(req.user._id) > -1)) {
             res.json(album);
